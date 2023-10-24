cd097edddc02aa30e207ac2f566a64ac6b91b024
wjunxxx <wjun@88.com>
2023-10-24 23:52:06 +0800

    feat: 项目配置

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
-import '@/assets/style.css'
+import '@/assets/style/style.css'
+import '@/assets/style/global.scss'
 import App from './App.vue'
-import router from "./router/index.ts";
+import router from "@/router";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,3 +1,41 @@
 <template>
-  模板项目首页
+  <div class="container">
+    <div class="menu">
+      <div class="menu-group" v-for="(item, index) in menu" :key="index">
+        <div class="menu-group-name">{{ item.name }}</div>
+        <div class="flex align-items-center">
+          <el-button type="primary" v-for="(ite, inx) in item.children" :key="inx">{{ ite.name }}</el-button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const menu = ref([
+  {
+    name: '示例',
+    children: [
+      { name: '组件', path: '/component' },
+      { name: '组件', path: '/component' },
+      { name: '组件', path: '/component' },
+      { name: '组件', path: '/component' },
+    ]
+  }
+])
+</script>
+
+<style scoped lang='scss'>
+.container {
+  padding: 20px;
+
+  .menu {
+    &-group {
+      &-name {
+        margin: 0 0 20px 0;
+      }
+    }
+  }
+}
+</style>
