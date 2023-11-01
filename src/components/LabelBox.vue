43f2e8761b94d1ef149625b645419fe2e0ea0dd1
wangjun <wangjun@guishangyi.cn>
2023-11-01 17:04:22 +0800

    feat: 集成vueuse

--- src/components/LabelBox.vue
+++ src/components/LabelBox.vue
@@ -1,5 +1,17 @@
-<template>
-  <div class="label-box">
+<script setup lang="ts">
+  import { useMouse, useDark, useTitle, debounceFilter } from "@vueuse/core";
+  const { x, y } = useMouse({ eventFilter: debounceFilter(100) });
+  const isDark = useDark();
+  
+</script>
 
+<template>
+  <div>useMouse(鼠标位置): {{ x }}, {{ y }}</div>
+  <div>useDark(黑暗模式): {{ isDark }}</div>
+  <div>
+    useTitle(修改标题):
+    <a @click="useTitle('标题修改测试')">修改标题为：标题修改测试</a>
   </div>
 </template>
+
+<style lang="scss" scoped></style>
